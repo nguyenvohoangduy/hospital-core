9717ef2bf30e5a9a6734f26f7ec4bb392d64efd0
Duy Nguyen <nguyenvohoangduy@gmail.com>
2019-03-22 04:02:21 +0000

    HM-238 Lưu thông tin lần cuối đăng nhập thêm file thiếu

--- app/Http/Controllers/Api/V1/Auth/AuthController.php
+++ app/Http/Controllers/Api/V1/Auth/AuthController.php
@@ -55,6 +55,7 @@ class AuthController extends APIController
                 'msg' => 'Invalid Credentials.'
             ], 400);
         }
+        $this->authService->updateLoginDate($request->email);
         $data = $this->authService->getUserRolesByEmail($request->email);
         $userName = $this->authService->getUserNameByEmail($request->email);
         $extraPayload = array(
