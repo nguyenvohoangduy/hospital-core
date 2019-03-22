bf2f9f3ad078068810051b7a89f6f2aa01bb6194
hungbg93 <nguyenvohoangduy@gmail.com>
2019-03-22 09:07:00 +0000

    edit

--- app/Repositories/PhieuChamSoc/PhieuChamSocRepository.php
+++ app/Repositories/PhieuChamSoc/PhieuChamSocRepository.php
@@ -27,13 +27,9 @@ class PhieuChamSocRepository extends BaseRepositoryV2
     {
         $column=[
             'phieu_cham_soc.*',
-            'khoa.ten_khoa',
-            'phong.ten_phong',
             'auth_users.fullname as ten_nguoi_tao'
             ];
         $data = $this->model
-            ->leftJoin('khoa','khoa.id','=','phieu_cham_soc.khoa_id')
-            ->leftJoin('phong','phong.id','=','phieu_cham_soc.phong_id')
             ->leftJoin('auth_users','auth_users.id','=','phieu_cham_soc.auth_users_id')
             ->where('phieu_cham_soc.dieu_tri_id',$dieuTriId)
             ->orderBy('phieu_cham_soc.id','ASC')
