e97c271158b0cd9fb7d97eb9ffeb9d8b645d4019
Duy Nguyen <nguyenvohoangduy@gmail.com>
2019-03-26 04:05:01 +0000

    API them xoa sua benh vien

---
+++ app/Http/Controllers/Api/V1/BenhVien/BenhVienController.php
@@ -0,0 +1,85 @@
+<?php
+namespace App\Http\Controllers\Api\V1\BenhVien;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\APIController;
+use App\Services\BenhVienService;
+use App\Http\Requests\BenhVienFormRequest;
+
+class BenhVienController extends APIController {
+    public function __construct
+    (
+        BenhVienService $benhVienService
+    )
+    {
+        $this->benhVienService = $benhVienService;
+    }
+    
+    public function getPartial(Request $request)
+    {
+        $limit = $request->query('limit', 100);
+        $page = $request->query('page', 1);
+        $name = $request->query('name', '');
+        
+        $data = $this->benhVienService->getPartial($limit, $page, $name);
+        return $this->respond($data);
+    }
+    
+    public function find($id)
+    {
+        $isNumericId = is_numeric($id);
+        
+        if($isNumericId) {
+            $data = $this->benhVienService->find($id);
+        } else {
+            $this->setStatusCode(400);
+            $data = [];
+        }
+        
+        return $this->respond($data);
+    }
+    
+    public function create(BenhVienFormRequest $request)
+    {
+        $input = $request->all();
+        
+        $id = $this->benhVienService->create($input);
+        if($id) {
+            $this->setStatusCode(201);
+        } else {
+            $this->setStatusCode(400);
+        }
+        
+        return $this->respond([]);
+    }
+    
+    public function update($id, BenhVienFormRequest $request)
+    {
+        try {
+            $isNumericId = is_numeric($id);
+            $input = $request->all();
+            
+            if($isNumericId) {
+                $this->benhVienService->update($id, $input);
+            } else {
+                $this->setStatusCode(400);
+            }
+        } catch (\Exception $ex) {
+            return $ex;
+        }
+    }
+    
+    public function delete($id)
+    {
+        $isNumericId = is_numeric($id);
+        
+        if($isNumericId) {
+            $this->benhVienService->delete($id);
+            $this->setStatusCode(204);
+        } else {
+            $this->setStatusCode(400);
+        }
+        
+        return $this->respond([]);        
+    }
+}
--- app/Services/BenhVienService.php
+++ app/Services/BenhVienService.php
@@ -20,5 +20,32 @@ class BenhVienService{
         $data = $this->benhVienRepository->listBenhVien();
         return $data;
     }
-
+    
+    public function getPartial($limit, $page, $name)
+    {
+        $data = $this->benhVienRepository->getPartial($limit, $page, $name);
+        return $data;
+    }
+    
+    public function find($id)
+    {
+        return $this->benhVienRepository->find($id);
+    }
+    
+    public function create(array $input)
+    {
+        $id = $this->benhVienRepository->create($input);
+        return $id;
+    }
+    
+    public function update($id, array $input)
+    {
+        $result = $this->benhVienRepository->update($id, $input);
+        return $result;
+    }
+    
+    public function delete($id)
+    {
+        $this->benhVienRepository->delete($id);
+    }
 }
