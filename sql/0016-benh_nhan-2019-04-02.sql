e0812387d9a731987c8672209185001a9250edf7
kptpro <lap.huynh@robustaeng.com>
2019-04-24 16:37:48 +0000

    fix bug nhap khoa

--- sql/0016-benh_nhan-2019-04-02.sql
+++ sql/0016-benh_nhan-2019-04-02.sql
@@ -2,4 +2,8 @@ begin transaction;
 ALTER TABLE benh_nhan ADD COLUMN ma_so_thue varchar(50);
 ALTER TABLE benh_nhan ADD COLUMN so_cmnd varchar(50);
 ALTER TABLE benh_nhan ADD COLUMN ma_tiem_chung varchar(50);
+ALTER TABLE benh_nhan ADD COLUMN ghi_chu text;
+ALTER TABLE benh_nhan DROP COLUMN loai_nguoi_than;
+ALTER TABLE benh_nhan DROP COLUMN ten_nguoi_than;
+ALTER TABLE benh_nhan DROP COLUMN dien_thoai_nguoi_than;
 end;
