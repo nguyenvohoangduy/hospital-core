6625dba6cb7de0d8ac2235a346a3510e5fa944fb
Duy Nguyen <nguyenvohoangduy@gmail.com>
2019-04-25 09:31:35 +0000

    Lich su giao dich

--- app/Http/Controllers/Api/V1/ViDienTu/ViDienTuController.php
+++ app/Http/Controllers/Api/V1/ViDienTu/ViDienTuController.php
@@ -20,4 +20,24 @@ class ViDienTuController extends APIController
         $this->setStatusCode(201);
         return $this->respond([]);
     }
+    
+    public function getPartialBenhNhan(Request $request) {
+        $limit = $request->query('limit', 100);
+        $page = $request->query('page', 1);
+        $keyword = $request->query('keyword', '');
+        
+        $data = $this->viDienTuService->getPartial($limit, $page, $keyword);
+        return $this->respond($data);
+    }
+    
+    public function getListLichSuGiaoDichByBenhNhanId(Request $request) {
+        $limit = $request->query('limit', 100);
+        $page = $request->query('page', 1);
+        $from = $request->query('from', '');
+        $to = $request->query('to', '');
+        $benhNhanId = $request->query('benhNhanId');
+        
+        $data = $this->viDienTuService->getListLichSuGiaoDichByBenhNhanId($limit, $page, $benhNhanId, $from, $to);
+        return $this->respond($data);
+    }
 }
--- app/Repositories/ViDienTu/LichSuGiaoDichRepository.php
+++ app/Repositories/ViDienTu/LichSuGiaoDichRepository.php
@@ -3,6 +3,8 @@ namespace App\Repositories\ViDienTu;
 
 use App\Repositories\BaseRepositoryV2;
 use App\Models\LichSuGiaoDich;
+use App\Helper\Util;
+use Carbon\Carbon;
 
 class LichSuGiaoDichRepository extends BaseRepositoryV2
 {
@@ -15,5 +17,19 @@ class LichSuGiaoDichRepository extends BaseRepositoryV2
     {
         $id = $this->model->create($input)->id;
         return $id;
+    }
+    
+    public function getListByBenhNhanId($limit = 100, $page = 1, $benhNhanId, $from, $to) {
+        $query = $this->model->where('benh_nhan_id', '=', $benhNhanId);
+        
+        if($from == $to){
+            $query = $query->whereDate('ngay_giao_dich', '=', $from);
+        } else {
+            $query = $query->whereBetween('ngay_giao_dich', [Carbon::parse($from)->startOfDay(), Carbon::parse($to)->endOfDay()]);
+        }
+        
+        $data = $query->orderBy('id', 'desc');
+        
+        return Util::getPartial($data, $limit, $page);
     }
 }
