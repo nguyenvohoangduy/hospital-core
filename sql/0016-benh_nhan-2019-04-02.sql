8ca72d9628704a7176b36ac41e270765053e240b
kptpro <lap.huynh@robustaeng.com>
2019-04-25 02:11:41 +0000

    update .env.example, sql

--- sql/0016-benh_nhan-2019-04-02.sql
+++ sql/0016-benh_nhan-2019-04-02.sql
@@ -2,6 +2,7 @@ begin transaction;
 ALTER TABLE benh_nhan ADD COLUMN ma_so_thue varchar(50);
 ALTER TABLE benh_nhan ADD COLUMN so_cmnd varchar(50);
 ALTER TABLE benh_nhan ADD COLUMN ma_tiem_chung varchar(50);
+ALTER TABLE benh_nhan ADD COLUMN vi_dien_tu text;
 ALTER TABLE benh_nhan ADD COLUMN ghi_chu text;
 ALTER TABLE benh_nhan DROP COLUMN loai_nguoi_than;
 ALTER TABLE benh_nhan DROP COLUMN ten_nguoi_than;
